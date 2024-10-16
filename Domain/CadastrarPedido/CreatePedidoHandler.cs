115c7be523565bd9e51c26a015695d736ae24a91
Mateus Souza da Silva <mateuszsouza96@gmail.com>
2024-10-16 18:10:01 -0300

    cleaning

--- Domain/CadastrarPedido/CreatePedidoHandler.cs
+++ Domain/CadastrarPedido/CreatePedidoHandler.cs
@@ -2,11 +2,6 @@
 using Domain.Model;
 using Domain.Repository;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.CadastrarPedido
 {
